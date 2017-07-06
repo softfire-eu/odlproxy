027d34e0331dff4571c1ca479ded154e07d23f1f
Claudio Navisse <claudio.navisse@assembly.it>
2017-07-06 15:25:07 +0200

    Config Log
    Delete dependencis Nova Client
    Handler Exception
    Fix log

--- odlproxy/odl_proxy_listner.py
+++ odlproxy/odl_proxy_listner.py
@@ -1,3 +1,4 @@
+import oslo_concurrency
 import pika
 import json
 import os
@@ -17,33 +18,41 @@ def nova_callback(ch, method, properties, body):
     :param properties: refers to the proprieties of the message
     :param body: refers to the message transmitted
     """
+    try:
+        logger.info("--------------------------------------------------------- ")
+        logger.info("called callback... ")
+        logger.info("routing key=%s" % method.routing_key)
+        logger.info("exchange=%s" % method.exchange)
 
-    print '---------------------------------------------------------'
-    print 'called callback...'
-    print "routing key=%s" % method.routing_key
-    print "exchange=%s" % method.exchange
+        oslo_message = json.loads(body)
 
-    oslo_message = json.loads(body)
-    print oslo_message
-    event = oslo_message['event_type']
-    tenant_id = oslo_message['_context_tenant']
-    if event == "compute.instance.create.end":
-        #create the flow
-        server_id_create = oslo_message['payload']['instance_id']
-        odl_proxy_api.createFlowFromVM(server_id_create,tenant_id)
-        print "server_id_create : " +  server_id_create
-        print "create"
-    elif event == "compute.instance.delete.end":
-        #delete the flow
-        # TODO prendere il server ID corretto
-        server_id_delete = oslo_message['payload']['instance_id']
-        odl_proxy_api.deleteFlowFromVM(server_id_delete,tenant_id)
-        print "server_id_delete : " + server_id_delete
-        print "delete"
+        if oslo_message['oslo.message']:
+            oslo_message = oslo_message['oslo.message']
 
+        logger.info(oslo_message)
+
+        event = oslo_message['event_type']
+        tenant_id = oslo_message['_context_tenant']
+
+        if event == "compute.instance.create.end":
+            #create the flow
+            logger.info("event_type : " +event )
+            server_id_create = oslo_message['payload']['instance_id']
+            odl_proxy_api.createFlowFromVM(server_id_create,tenant_id)
+            logger.info("server_id_create : " +  server_id_create)
+        elif event == "compute.instance.delete.end":
+            #delete the flow
+            logger.info("event_type : " + event)
+            server_id_delete = oslo_message['payload']['instance_id']
+            odl_proxy_api.deleteFlowFromVM(server_id_delete,tenant_id)
+            logger.info("server_id_delete : " + server_id_delete)
+
+    except Exception as e:
+        msg = "ODL Proxy " + str(e)
+        return msg
 
 def listenerNotifications():
-    logger.info("create listener to rabbit")
+    logger.info("Create listener to rabbit")
 
     '''
     #transport_url = 'rabbit://stackrabbit:stackqueue@10.200.4.8:5672/'
@@ -77,31 +86,15 @@ def listenerNotifications():
     result = channel.queue_declare(exclusive=True)
     queue_name = result.method.queue
 
-    print "queue_name: %s" % queue_name
-
-    # channel.exchange_declare(exchange='heat', type='topic')
-    # using routing_key
-    # channel.queue_bind(exchange='openstack', queue=queue_name, routing_key='notifications.#')
-    # channel.queue_bind(exchange='openstack', queue=queue_name, routing_key='notifications.#')
-    # get all messages on heat
-    # channel.queue_bind(exchange='heat', queue=queue_name, routing_key='#')
-
     channel.queue_bind(exchange='nova', queue=queue_name, routing_key='notifications.#')
 
-    # channel.queue_bind(exchange='ceilometer', queue=queue_name, routing_key='#')
+    logger.info("queue_name: " + queue_name)
+    logger.info("exchange : nova")
+    logger.info("routing_key : notifications.#")
 
     channel.basic_consume(nova_callback, queue=queue_name, no_ack=True)
     channel.start_consuming()
-    print 'consuming started'
+    logger.info("consuming started")
 
-def odlListener_main():
-    print odl_proxy_api
-    print sys
-    print ConfigParser
-    print "odlListener_main"
 
-if __name__ == '__main__':
-    print __name__
-    odlListener_main()
-else:
-    print __name__
+
